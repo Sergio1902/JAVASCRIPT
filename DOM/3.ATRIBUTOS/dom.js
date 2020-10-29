f0abfc2fd6f4779c6e650cf8dab6e05b8fb4bcef
Sergio1902 <ser19car@gmail.com>
2020-10-29 17:58:29 -0500

    SELECTORES-ATRIBUTOS-CSS

---
+++ DOM/3.ATRIBUTOS/dom.js
@@ -0,0 +1,68 @@
+
+/* ================================================== */
+/* ==================  ATRIBUTOS ====================*/ 
+/* ================================================== */
+
+// obtener el valor de los atributos
+
+// 1era forma - todo el documento html - name atributo
+    console.log(document.documentElement.lang)
+// 2da forma
+    console.log(document.querySelector(".cards").getAttribute("class"))
+
+    const $link = document.querySelector(".link-dom").getAttribute("href")
+    console.log($link)
+
+
+// asignar un nuevo valor a los atributos
+
+// 1era forma
+    document.documentElement.lang = "es"
+    console.log(document.documentElement.lang)
+
+// 2da forma - setAttribute (atributo a cambiar, nuevo valor)
+    // se puede cambiar un valor existente 
+        
+    document.documentElement.setAttribute("lang","PERU")
+    console.log(document.documentElement.getAttribute("lang"))
+
+    // agregar un nuevo atributo
+    const $LINK = document.querySelector(".link-dom")
+    $LINK.setAttribute("target","_blank")
+
+/*
+dato --> para evitar el hack en otra pestaña
+rel = "noopener"
+*/
+    $LINK.setAttribute("rel","noopener")
+    console.log($LINK)
+
+// eliminar atributo - buscar atributo
+    console.log($LINK.hasAttribute("href"))
+    $LINK.removeAttribute("rel")
+
+
+/* ================================================== */
+/* ==================  DATA ATRIBUTOS ====================*/ 
+/* ================================================== */
+
+// const $CARD = document.querySelector(".card")
+// console.log($CARD.getAttribute("data-description"))
+// $CARD.setAttribute("data-description","HOLAAA")
+// console.log($CARD)
+
+/*
+existe un archivo MAP donde almacena los
+data-description -- DATASET
+*/
+// console.log($CARD.dataset.description)
+// console.log($CARD.hasAttribute("data-description"))
+
+
+/* ================================================== */
+/* ============  ESTILOS Y VARIABLES CSS ============*/ 
+/* ================================================== */
+
+
+
+
---
+++ DOM/4.CSS/dom.js
@@ -0,0 +1,45 @@
+/* ================================================== */
+/* ============  ESTILOS CSS ============*/ 
+/* ================================================== */
+
+const $LINK_DOM = document.querySelector(".link-dom")
+// solo obtiene los estilos asignados por el programador
+console.log($LINK_DOM.getAttribute("style"))
+// obtine todos los estilos en un objeto map
+console.log($LINK_DOM.style)
+// propiedades internas de un atributo .
+console.log($LINK_DOM.style.backgroundColor)
+
+// ver los estilos directos desde el objeto windows
+console.log(window.getComputedStyle($LINK_DOM))
+// propiedades internas de un atributo .
+console.log(window.getComputedStyle($LINK_DOM).getPropertyValue("background-color"))
+
+// agregar nuevos estilos
+$LINK_DOM.style.setProperty("text-decoration","none")
+$LINK_DOM.style.setProperty("display","block")
+$LINK_DOM.style.width = "50%"
+$LINK_DOM.style.setProperty("text-align","center")
+$LINK_DOM.style.padding ="20px"
+$LINK_DOM.style.setProperty("border-radius","15px")
+
+/*/
+recuerda el getPropertyValue solo se utliza con getComputedStyle
+*/
+/* ================================================== */
+/* ============  VARIABLES CSS ============*/ 
+/* ================================================== */
+
+const $HTML = document.documentElement
+const $BODY = document.body
+
+let varDarkColor = window.getComputedStyle($HTML).getPropertyValue("--dark-color")
+let varYellowColor = window.getComputedStyle($HTML).getPropertyValue("--yellow-color")
+
+$BODY.style.color = varYellowColor
+$BODY.style.background = varDarkColor
+
+// modificar la variable css desde el DOM
+$HTML.style.setProperty("--dark-color","pink")
+varDarkColor = window.getComputedStyle($HTML).getPropertyValue("--dark-color")
+$BODY.style.setProperty("background-color",varDarkColor)
