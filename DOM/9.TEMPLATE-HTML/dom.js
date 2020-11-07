16a9443fd5ecf9f0de94c315e60f7e4a33abedfe
Sergio1902 <ser19car@gmail.com>
2020-11-06 19:17:28 -0500

    FRAGMENTO-CLON

---
+++ DOM/9.TEMPLATE-HTML/dom.js
@@ -0,0 +1,45 @@
+/* ================================================== */
+/* =============  TEMPLATE HTML =====================*/ 
+/* ================================================== */
+
+// estas etiquetas no se visualizan solo en consola
+
+const $cards = document.querySelector(".cards")
+
+// ingresar al contenido de la etiqueta template
+const $template = document.getElementById("template-card").content
+const $fragment = document.createDocumentFragment()
+const 
+
+// arreglo del card
+cardContent = [
+    {   title: "Tecnología",
+        img:"https://placeimg.com/200/200/tech"
+    },
+    {   title: "Animales",
+        img:"https://placeimg.com/200/200/animals"
+    },
+    {   title: "Arquitectura",
+        img:"https://placeimg.com/200/200/arch"
+    },
+    {   title: "Gente",
+        img:"https://placeimg.com/200/200/people"
+    },
+    {   title: "Naturaleza",
+        img:"https://placeimg.com/200/200/nature"
+    }
+]
+
+cardContent.forEach(element => {
+
+    // seleccionar a la img y figcaption y le agrega atributos
+    $template.querySelector("img").setAttribute("src",element.img)
+    $template.querySelector("img").setAttribute("alt",element.title)
+    $template.querySelector("figcaption").textContent = element.title
+
+    // crear un clon del templete creado para agregar 
+    let $clon = document.importNode($template,true)
+    $fragment.appendChild($clon)
+});
+
+$cards.appendChild($fragment)
