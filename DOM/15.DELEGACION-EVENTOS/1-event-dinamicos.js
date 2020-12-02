e1e0f40db3c21f986b423e11d61f038ea12577ea
Sergio1902 <ser19car@gmail.com>
2020-12-01 23:51:24 -0500

    eventos-dinamicos-DOM

---
+++ DOM/15.DELEGACION-EVENTOS/1-event-dinamicos.js
@@ -0,0 +1,47 @@
+// forma correcta de asignar - asignar eventos dinamicamente
+/*
+asignarlos al document - y luego designar eventos a cada 
+elemento seleccionado
+*/
+
+function flujoEventos(e) {
+    console.log(`soy el div ${this} - origino el evento el div ${e.target.className} `)
+    e.stopPropagation()
+}
+/*
+manejador de eventos al document 
+    saber a quien le he dado click dentro del DOM
+*/
+document.addEventListener("click",function (e) {
+    console.log("click fue dado en " , e.target)
+
+    // linkeventos 
+    /*
+    matches - devuelve boleean - al comparar con un parametro
+    especial
+    */
+    if(e.target.matches(".eventos-flujo a")){
+        alert("HOLA SOY SERGIO")
+        e.preventDefault()
+    }
+
+    // div
+    /*
+    asignar a cada div un la funcion flujoEventos()
+    */
+
+    if(e.target.matches(".eventos-flujo div")){
+        flujoEventos(e)
+    }
+
+
+})
+
+
+/*
+$divEventos.forEach((div) => {
+    div.addEventListener("click",flujoEventos)
+    
+});
+*/
+
