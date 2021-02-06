72fa58891eb3391cdfed962d3dba79f57d97646f
Sergio1902 <ser19car@gmail.com>
2021-02-05 20:15:53 -0500

    form-correo

--- 43.FORMULARIO/js/validaciones_form.js
+++ 43.FORMULARIO/js/validaciones_form.js
@@ -2,7 +2,6 @@ export default function contactFormValidations() {
     const $FORM = document.querySelector(".contact-form")
     const $IMPUT = document.querySelectorAll(".contact-form [required]")
     
-    console.log($IMPUT)
     $IMPUT.forEach(function (element) {     
         const $SPAN = document.createElement("span")
         // relacionarlo al imput a traves de id
@@ -10,7 +9,7 @@ export default function contactFormValidations() {
         // asiganarle el texto errors
         $SPAN.textContent = element.title
 
-        $SPAN.classList.add(".contact-form-error", "none")
+        $SPAN.classList.add("contact-form-error", "none")
 
         // ubicacion
         element.insertAdjacentElement("afterend", $SPAN)
@@ -23,18 +22,40 @@ export default function contactFormValidations() {
             
             let patter = e.target.pattern || e.target.dataset.pattern
             
-            if(patter){
+            if(patter && e.target.value !==""){
                 // new expresion regular
                 let expReg = new RegExp(patter)
-                console.log(expReg)
-                return expReg.exec(e.target.value)
-                ? document.getElementById(e.target.name).classList.remove("is-active")
-                : document.getElementById(e.target.name).classList.add("is-active")
-        
+                return !expReg.exec(e.target.value)
+                ? document.getElementById(e.target.name).classList.add("is-active-form")
+                : document.getElementById(e.target.name).classList.remove("is-active-form")
             }
-            
+            if(e.target.value === ""){
+                document.getElementById(e.target.name).classList.remove("is-active-form")
+            }
+            if(!patter){
+                return e.target.value === ""
+                ? document.getElementById(e.target.name).classList.add("is-active-form")
+                : document.getElementById(e.target.name).classList.remove("is-active-form")
+            }
+
         }
+    })
+    document.addEventListener("submit", function (e) {
+        // e.preventDefault()
         
+        const $LOADER = document.querySelector(".contact-form-loader")
+        const $MESAGE = document.querySelector(".contact-form-response")
+
+        $LOADER.classList.remove("none")
+        setTimeout(() => {
+            $LOADER.classList.add("none")
+            $MESAGE.classList.remove("none")
+
+            setTimeout(() => {
+                $MESAGE.classList.add("none")
+            }, 3000);
+
+        }, 3000);
 
     })
 
